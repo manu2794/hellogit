5c5667251a0eb7773ec7e58fc113d10ba9025e07
Michele Bitetto <michele17121994@gmail.com>
2017-04-12 17:29:45 +0200

    Modificato da Michele

--- hellogit/src/it/uniroma3/prova/HelloWorld.java
+++ hellogit/src/it/uniroma3/prova/HelloWorld.java
@@ -3,5 +3,6 @@ package it.uniroma3.prova;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!");
+		System.out.println("Hello World by Michele");
 	}
 }
